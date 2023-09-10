93ca0ad0324a255a4ae5ec06a6344b6dbc0ff250
elajasho <elaja.sho@gmail.com>
2023-09-10 18:18:20 +0300

    added stupid getmessage() method

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,6 +2,10 @@ package guru.qa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa guru!");
+
+        System.out.println(getmessage());
+    }
+    private static String getmessage()  {
+        return "Hello qa guru!";
     }
 }
